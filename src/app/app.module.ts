37e7b9bcf0be596a0460fcea6abdff771c9a2cbf
DominicSilva <100739614+DominicSilva@users.noreply.github.com>
2022-05-16 21:43:46 -0500

    pepe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    LoginComponent,
+    RegistroComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
